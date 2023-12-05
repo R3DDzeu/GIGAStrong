18b787695ec4af2980cf1a2f5187d606e4eaafa3
Ion-Alexandru Rosu <rosuionalexandru@gmail.com>
2023-12-05 19:01:51 +0200

    added TODOs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,5 +11,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	//TODO: add getExerciseByUserID & getUsersByExerciseID
+	//TODO: implement & complete all unfinished methods
+	//TODO: Complete the UnitTests
+	//TODO: Remake IntegrationTest
+	//TODO: Clear useless code snippets
+	//TODO: Add more exceptions and implement them
+	//TODO: Add validations to HTTP Requests
+	//TODO: ADD new Workout, Routine and Logs entities
 }
